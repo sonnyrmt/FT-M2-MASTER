d030e702b1beacb6b4398e164735bfa74dc597d1
unknown <santi2357@hotmail.com>
2022-07-20 19:48:39 -0300

    wk done

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,16 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  const {max,min,name,img,onClose} = props;
+  const maxCelsius = Math.round(max - 273); 
+  const minCelsius = Math.round(min - 273);
+  const image = `http://openweathermap.org/img/wn/${img}@2x.png`;
+
+  return <div className='container-weather'>
+    <button onClick={onClose}>X</button>
+    <h1>{name}</h1>
+    <p>Min {minCelsius}°</p>
+    <p>Max {maxCelsius}°</p>
+    <img src={image} alt="weatherImg" />
+  </div>
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return <div>
+    <input type='text' placeholder='Ciudad..'></input>
+    <button onClick={props.onSearch}>Agregar</button>
+  </div>
 };
